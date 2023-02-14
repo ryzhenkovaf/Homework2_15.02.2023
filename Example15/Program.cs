3f6e76e402baebd012a268ce8061d8d3f09413aa
Aleksei Ryzhenkov <ryzhenkovaf@gmail.com>
2023-02-14 20:12:33 +0700

    add 15

---
+++ Example15/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите день недели: ");
+int day = int.Parse(Console.ReadLine());
+
+if (day >= 1 && day <= 5)
+    Console.WriteLine("НЕТ");
+if (day >= 6 && day <= 7)
+    Console.WriteLine("ДА");
+if (day > 7)
+    Console.WriteLine("Это не день недели");
